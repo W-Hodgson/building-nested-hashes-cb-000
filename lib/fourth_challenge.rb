0879e312070f2fdd7a1a7dfffbd927d7b7f8c687
Will Hodgson <will.hodgson@current-rms.com>
2019-07-30 09:48:24 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {:name => "Benvolio", :age => "17", :attitude => "worried"},
-        {:name => "Metcutio"}
+        {:name => "Metcutio", :age => "18", :attitude => ""}
       ]
    },
    :capulet => {
